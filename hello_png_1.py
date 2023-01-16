84826ea2fb6ae91ed0eae240ee927e74373ac875
David Buchanan <d@vidbuchanan.co.uk>
2023-01-16 21:09:26 +0000

    Update

--- hello_png_1.py
+++ hello_png_1.py
@@ -4,7 +4,7 @@ import zlib
 PNG_SIGNATURE = b'\x89PNG\r\n\x1a\n'
 
 # https://www.w3.org/TR/2022/WD-png-3-20221025/#dfn-png-four-byte-unsigned-integer
-# Helper function to pack an int into a PNG 4-byte unsigned integer
+# Helper function to pack an int into a "PNG 4-byte unsigned integer"
 def encode_png_uint31(value):
 	if value > 2**31 - 1:  # This is unlikely to ever happen!
 		raise ValueError("Too big!")
@@ -43,7 +43,7 @@ def encode_png_ihdr(
 
 	return ihdr
 
-# This is all the code required to read subpixel values from an ".rgb" file
+# This is all the code required to read subpixel values from an ".rgb" file.
 # subpixel 0=R, 1=G, 2=B
 def read_rgb_subpixel(rgb_data, width, x, y, subpixel):
 	return rgb_data[3 * ((width * y) + x) + subpixel]
@@ -56,7 +56,7 @@ def apply_png_filters(rgb_data, width, height):
 	# we'll work with an array of ints, and convert to bytes at the end
 	filtered = []
 	for y in range(height):
-		filtered.append(0) # Filter type 0 (none!)
+		filtered.append(0) # Always filter type 0 (none!)
 		for x in range(width):
 			filtered += [
 				read_rgb_subpixel(rgb_data, width, x, y, 0), # R
@@ -67,15 +67,17 @@ def apply_png_filters(rgb_data, width, height):
 
 
 if __name__ == "__main__":
-	# Load the input data:
-	# (these values are hardcoded because the ".rgb" "format" has no metadata)
+	# These values are hardcoded because the .rgb "format" has no metadata
 	INPUT_WIDTH = 320
 	INPUT_HEIGHT = 180
-	input_rgb_data = open("./samples/hello_png.rgb", "rb").read() # read entire file as bytes
+	# read entire file as bytes
+	input_rgb_data = open("./samples/hello_png.rgb", "rb").read()
 
 	ihdr = encode_png_ihdr(INPUT_WIDTH, INPUT_HEIGHT)
 
 	filtered = apply_png_filters(input_rgb_data, INPUT_WIDTH, INPUT_HEIGHT)
+
+	# Apply zlib compression
 	idat = zlib.compress(filtered, level=9) # level 9 is maximum compression!
 
 	with open("samples/out_1.png", "wb") as f: # open file for writing
